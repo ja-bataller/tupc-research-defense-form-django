6af633443b5a1276031d2a2903a952ddfe6f9969
John Bataller <johnanthonybataller.ce@gmail.com>
2022-07-13 11:47:00 +0800

    initial commit

---
+++ tupc_research_defense_form_web_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TupcResearchDefenseFormWebAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tupc_research_defense_form_web_app'
