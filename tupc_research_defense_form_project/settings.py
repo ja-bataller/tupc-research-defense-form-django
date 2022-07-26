b307e254d54cb65944280ae051a89c7bc49fe03a
John Bataller <johnanthonybataller.ce@gmail.com>
2022-07-26 18:53:49 +0800

    refactor heroku settings

--- tupc_research_defense_form_project/settings.py
+++ tupc_research_defense_form_project/settings.py
@@ -46,6 +46,9 @@ AUTH_USER_MODEL = 'tupc_research_defense_form_web_app.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
